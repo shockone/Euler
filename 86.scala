6706c2cad48d0ccdb9bf8b14097f4053fb8a42df
unknown <v.shatsky@support01.m1.te.ua>
2013-03-18 23:50:35 +0200

    86

--- 86.scala
+++ 86.scala
@@ -1,9 +1,3 @@
-val m = 100
-
-def shortestRouteIsInteger(x: Int, y: Int, z: Int) = {
-
-}
-
 def findShortestRoute(x: Int, y: Int, z: Int) = {
   List(
   math.sqrt(x*x + (y+z) * (y+z)),
@@ -12,4 +6,29 @@ def findShortestRoute(x: Int, y: Int, z: Int) = {
   ).min
 }
 
-Console println findShortestRoute(6, 5, 3)
+def isInteger(x: Double) = x == math.floor(x)
+
+var m = 1000000
+var x = 1
+var y = 1
+var z = 1
+var total = 0
+
+while (total <= m) {
+	Console println total
+	while (y <= x) {
+		while (z <= y) {
+			if (isInteger(findShortestRoute(x, y, z))) {
+				total += 1
+			}
+			z += 1
+		}
+		y += 1
+		z = 1
+	}	
+	x += 1
+	y = 1
+}
+
+
+Console println x - 1
